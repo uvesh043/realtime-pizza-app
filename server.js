8bddd1eafa2d5d9556c8875f58bf8be871b5ec37
uvesh <nizamumair96@gmail.com>
2021-05-02 13:13:27 +0530

    this is intail commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const app=express();
+const hbs=require('hbs');
+const port=process.env.PORT || 3000
+const path=require('path')
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname,"/resources/views/"))
+app.get('/',(req,res)=>{
+    // res.send("Hello from server")
+    res.render('home')
+})
+app.listen(port,()=>{
+console.log(`Listening on ${port}`);
+})
