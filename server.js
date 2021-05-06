04ebb16d42376d44e62f835e467758338d4e87f5
uvesh <nizamumair96@gmail.com>
2021-05-06 11:17:17 +0530

    add cart page,authentication page(login,register page)

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express=require("express");
-const app=express();
-const hbs=require('hbs');
-const port=process.env.PORT || 3000
-const path=require('path')
+const express = require("express");
+const { registerPartial } = require("hbs");
+const app = express();
+const hbs = require('hbs');
+const port = process.env.PORT || 3000
+const path = require('path')
 app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname,"/resources/views/"))
+app.set('views', path.join(__dirname, "/resources/views/"))
 app.use(express.static('public'))
-app.get('/',(req,res)=>{
+//register parials folder
+const partialsPath = path.join(__dirname, '/resources/views/partials')
+hbs.registerPartials(partialsPath);
+app.get('/', (req, res) => {
     // res.send("Hello from server")
     res.render('home')
 })
-app.listen(port,()=>{
-console.log(`Listening on ${port}`);
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
 })
