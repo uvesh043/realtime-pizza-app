a8393b3278ba39802603532905f0313751884731
uvesh <nizamumair96@gmail.com>
2021-05-09 22:56:16 +0530

    creating file after .env file

--- server.js
+++ server.js
@@ -1,28 +1,45 @@
 const express = require("express");
-const { registerPartial } = require("hbs");
 const app = express();
-const hbs = require('hbs');
-const port = process.env.PORT || 3000
-const path = require('path')
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, "/resources/views/"))
-app.use(express.static('public'))
-//register parials folder
-const partialsPath = path.join(__dirname, '/resources/views/partials')
-hbs.registerPartials(partialsPath);
-app.get('/', (req, res) => {
-    // res.send("Hello from server")
-    res.render('home')
-})
-app.get('/cart', (req, res) => {
-    res.render('customers/cart')
-})
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
+const ejs=require('ejs');
+require('dotenv').config();
+const session=require('express-session');
+const expressLayout=require('express-ejs-layouts');
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const flash=require('express-flash');
+mongoose.connect('mongodb://localhost:27017/pizza-app', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+
+}).then(() => {
+    console.log("connection succesful");
+}).catch((e) => {
+    console.log('No conection ');
+});
+
+
+app.use(session({
+    secret:process.env.COOKIE_SECRET, 
+    resave:false,
+    saveUninitialized:false,
+    cookie:{maxAge:1000*60*60*24} // 24 hours
+}))
+const path = require('path');
+
+app.use(flash())
+app.use(expressLayout);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "/resources/views/"));
+app.use(express.static('public'));
+
+
+
+// require for all routes
+const routes = require('./routes/web');
+routes(app);
+
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
 })
